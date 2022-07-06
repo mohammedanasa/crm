d2f32f1f87161da17fb6aee5f6fec31cde168974
Mohammed Anas Alambilassery <mohammedanasa@outlook.com>
2022-07-06 15:14:30 +0100

    Rendered Data to Dashboard

--- accounts/views.py
+++ accounts/views.py
@@ -1,12 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from accounts.models import Customer, Product, Order
+
 
 def home(request):
-    return render(request,'accounts/dashboard.html')
+    orders = Order.objects.all()
+    customers=Customer.objects.all()
+    total_customer = customers.count()
+    total_order = orders.count()
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    context = {'orders':orders,'customers':customers,'total_order':total_order,'delivered':delivered, 'pending':pending}
+    return render(request,'accounts/dashboard.html', context)
 
 def products(request):
-    return render(request,'accounts/products.html')
+    products = Product.objects.all()
+    return render(request,'accounts/products.html', {'products':products})
 
 def customer(request):
     return render(request,'accounts/customers.html')
